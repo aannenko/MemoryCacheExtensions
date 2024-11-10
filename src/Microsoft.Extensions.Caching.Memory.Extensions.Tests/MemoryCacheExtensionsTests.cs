313bd4efc4d752600f9351af6ffdf1a8533b64d2
Andrii Annenko <roxton.public@gmail.com>
2024-11-10 18:19:06 +0100

    Update unit tests

--- src/Microsoft.Extensions.Caching.Memory.Extensions.Tests/MemoryCacheExtensionsTests.cs
+++ src/Microsoft.Extensions.Caching.Memory.Extensions.Tests/MemoryCacheExtensionsTests.cs
@@ -14,16 +14,16 @@ public class Tests
         cache.GetOrCreate(1, ce => "one");
         cache.GetOrCreate("two", ce => "two");
 
-        IEnumerable? keysCollection = null;
-        Assert.DoesNotThrow(() => keysCollection = cache.GetKeys());
+        IEnumerable? allKeysEnumerable = null;
+        Assert.DoesNotThrow(() => allKeysEnumerable = cache.GetKeys());
 
-        Assert.That(keysCollection, Is.Not.Null);
-        Assert.That(keysCollection, Is.EquivalentTo(_expectedKeysCollection));
+        Assert.That(allKeysEnumerable, Is.Not.Null);
+        Assert.That(allKeysEnumerable, Is.EquivalentTo(_expectedKeysCollection));
 
-        IEnumerable<string>? keysEnumerable = null;
-        Assert.DoesNotThrow(() => keysEnumerable = cache.GetKeys<string>());
+        IEnumerable<string>? stringKeysEnumerable = null;
+        Assert.DoesNotThrow(() => stringKeysEnumerable = cache.GetKeys<string>());
 
-        Assert.That(keysEnumerable, Is.Not.Null);
-        Assert.That(keysEnumerable, Is.EquivalentTo(_expectedKeysEnumerable));
+        Assert.That(stringKeysEnumerable, Is.Not.Null);
+        Assert.That(stringKeysEnumerable, Is.EquivalentTo(_expectedKeysEnumerable));
     }
 }
